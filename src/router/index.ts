803e6ab4fe9f2d3cd2e17bc2e779bf21179c0fbb
Gustavo Cabreira <gustavo.softdev@gmail.com>
2025-03-02 19:22:23 -0300

    refactor: refactor router file

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,26 +1,5 @@
 import { createRouter, createWebHistory } from "vue-router";
-import Login from "../views/auth/Login.vue";
-import Contacts from "../views/Contacts.vue";
-import AppLayout from "../views/layouts/AppLayout.vue";
-
-const routes = [
-  {
-    path: '/auth/login',
-    name: 'Login',
-    component: Login,
-  },
-  {
-    path: '',
-    component: AppLayout,
-    children: [
-      {
-        path: '',
-        name: 'Contacts',
-        component: Contacts,
-      }
-    ]
-  }
-]
+import routes from "./routes";
 
 const router = createRouter({
   history: createWebHistory(),
